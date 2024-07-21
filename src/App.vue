6482ad05200286cd69868d72b6afb006cbe180c5
Abhinavvishwakarm <157105478+Abhinavvishwakarm@users.noreply.github.com>
2024-07-21 12:51:44 +0530

    Add files via upload
    Vue3 Demo project setup

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <v-app>
+      <v-main>
+        <v-btn class="vuet" color="primary">Hello, Vuetify!</v-btn>
+    <router-link to="/">Home</router-link>
+    <router-link to="/about">About</router-link>
+    <router-view></router-view>
+  </v-main>
+
+  </v-app>
+
+</template>
+
+<script>
+export default {
+  name: 'App'
+};
+</script>
+<style>
+@import 'vuetify/styles';
+</style>
---
+++ src/components/Home.vue
@@ -0,0 +1,31 @@
+
+<template>
+  <div>
+    <h1>Home</h1>
+    <nav>
+      <router-link to="/" @click="increment">Increment</router-link> |
+      <router-link to="/decrement" @click="decrement">Decrement</router-link>
+    </nav>
+    <router-view/> 
+    <p>Current Count: {{ count }}</p>
+  </div>
+</template>
+
+<script>
+import { mapGetters, mapActions } from 'vuex';
+
+export default {
+  name: 'homeComp',
+  computed: {
+    ...mapGetters({
+      count: 'getCount'
+    })
+  },
+  methods: {
+    ...mapActions({
+      increment: 'increment',
+      decrement: 'decrement'
+    })
+  }
+};
+</script>
